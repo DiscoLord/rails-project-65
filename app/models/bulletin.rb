a6efc9600d631c9e0e57d6ccfeda521ea3eb4beb
Boris Nikolaev <boris28596@gmail.com>
2025-03-11 17:03:47 +0100

    changed order of things in bulletin model

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -3,6 +3,17 @@
 class Bulletin < ApplicationRecord
   include AASM
 
+  belongs_to :category
+  belongs_to :user
+
+  has_one_attached :image
+  include ImageValidations
+
+  validates :category_id, numericality: { only_integer: true }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :image, presence: true
+
   aasm column: 'state' do
     state :draft, initial: true
     state :under_moderation
@@ -26,17 +37,6 @@ class Bulletin < ApplicationRecord
       transitions from: %i[draft published rejected under_moderation], to: :archived
     end
   end
-
-  has_one_attached :image
-  include ImageValidations
-
-  belongs_to :category
-  belongs_to :user
-
-  validates :category_id, numericality: { only_integer: true }
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :image, presence: true
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[title category_id state]
