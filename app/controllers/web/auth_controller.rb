0b9fecdb47250cc5156f25b1babde3d493d3a930
Boris Nikolaev <boris28596@gmail.com>
2025-03-11 21:05:06 +0100

    added more translations and hid useless navigation when logged out

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -8,14 +8,14 @@ module Web
       user.name = auth_data['info']['name'] || user.name
       user.save!
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Добро пожаловать, #{user.name}!"
+      redirect_to root_path, notice: t('.welcome', name: user.name)
     rescue StandardError => e
-      redirect_to root_path, alert: "Ошибка входа: #{e.message}"
+      redirect_to root_path, alert: t('.error', message: e.message)
     end
 
     def destroy
       session[:user_id] = nil
-      redirect_to root_path, notice: t('logged_out')
+      redirect_to root_path, notice: t('.logged_out')
     end
   end
 end
