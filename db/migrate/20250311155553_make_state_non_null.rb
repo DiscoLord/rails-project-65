73a3b6e105e11cb08fbea2cb1622b2c416f27bee
Boris Nikolaev <boris28596@gmail.com>
2025-03-11 20:24:02 +0100

    added from to to migration

--- db/migrate/20250311155553_make_state_non_null.rb
+++ db/migrate/20250311155553_make_state_non_null.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class MakeStateNonNull < ActiveRecord::Migration[7.2]
   def change
-    change_column_default :bulletins, :state, 'draft'
+    change_column_default :bulletins, :state, from: nil, to: 'draft'
     change_column_null :bulletins, :state, false
   end
 end
